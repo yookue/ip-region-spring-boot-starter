96765e74bf86911593b4c4135ec229048f21f413
David Hsing <davidhsing@163.com>
2025-08-15 18:43:36 +0800

    Update ValueEnum

--- src/main/java/com/yookue/springstarter/ipregion/enumeration/IpRegionSegmentType.java
+++ src/main/java/com/yookue/springstarter/ipregion/enumeration/IpRegionSegmentType.java
@@ -17,7 +17,7 @@
 package com.yookue.springstarter.ipregion.enumeration;
 
 
-import com.yookue.commonplexus.javaseutil.support.ValueEnum;
+import com.yookue.commonplexus.javaseutil.enumeration.ValueEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
